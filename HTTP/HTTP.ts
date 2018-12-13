2b9a4ad6ccbc2e50cd7c63879c28f7b20adcce76
awa2 <tawatsu@c-fo.com>
2018-12-13 10:03:42 +0900

    Create EventObject types and add Response class.

--- HTTP/HTTP.ts
+++ HTTP/HTTP.ts
@@ -1,24 +1,40 @@
+export namespace EventObject {
+    export type Request = {
+        queryString?: string | null,
+        parameter?: Object
+        parameters?: {[key: string]: Array<Object>},
+        contextPath?: string,
+        contentLength?: number,
+        postData?: {
+            length: number,
+            type: string,
+            contents: string,
+            name: string
+        }
+    }
+}
+
 export class Response {
-    constructor(){
+    constructor() {
 
     }
-    public json(){}
-    public type(){}
-    public render(){}
-    public send(){}
+    public json() { }
+    public type() { }
+    public render() { }
+    public send() { }
 }
 export class Request {
     // baseUrl? : string;
-    public body? : string;
-    public method : 'POST' | 'GET';
-    public originUrl : string;
+    public body?: string;
+    public method: 'POST' | 'GET';
+    public originUrl: string;
     // params : string;
-    public path : string;
-    public protocol : 'https';
-    public query : { [key : string] : any }
-    
-    constructor(e: any){
-        if(e['postData']){
+    public path: string;
+    public protocol: 'https';
+    public query: { [key: string]: any }
+
+    constructor(e: EventObject.Request) {
+        if (e['postData']) {
             switch (e.postData['contents']) {
                 case 'application/json':
                     this.body = JSON.parse(e.postData.contents);
@@ -38,6 +54,6 @@ export class Request {
     }
 
 }
-export function httpRequest(e: any){
+export function httpRequest(e: EventObject.Request) {
     return new Request(e);
 }
